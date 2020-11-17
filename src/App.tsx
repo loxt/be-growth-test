5cc8ba61fb3ac90c2400aacc41cd25444edff2ac
loxt <emannuelloxt@hotmail.com>
2020-11-17 16:29:16 -0400

    styles(home): fix card body font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="container-title">
+        <span>Be</span> Growth
+      </h1>
+      <div className="albums">
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+        <div className="album">
+          <div className="album-thumbnail" />
+          <h2 className="album-id">1</h2>
+          <p className="album-quantity">30 fotos</p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
